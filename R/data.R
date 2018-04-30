c4d777a8f45e7859e2187117d56603a3bbeef8d2
nsheff <nsheff@users.noreply.github.com>
2018-04-30 11:14:50 -0400

    add data note

--- R/data.R
+++ R/data.R
@@ -34,6 +34,7 @@ NULL
 #' @name chromSizes_mm10
 NULL
 
+
 #' TSS locations
 #'
 #' A dataset containing chromosome sizes for common reference genome assemblies
@@ -52,7 +53,6 @@ NULL
 #' @name TSS_hg38
 NULL
 
-
 #' TSS locations
 #'
 #' A dataset containing chromosome sizes for common reference genome assemblies
@@ -62,6 +62,15 @@ NULL
 #' @name TSS_mm10
 NULL
 
+#' TSS locations
+#'
+#' A dataset containing chromosome sizes for common reference genome assemblies
+#'
+#' @format A named vectors of lengths with one item per chromosome
+#' @source BSGenome package
+#' @name TSS_mm9
+NULL
+
 
 #' Gene models
 #'
@@ -69,7 +78,16 @@ NULL
 #'
 #' @format A named vectors of lengths with one item per chromosome
 #' @source BSGenome package
-#' @name geneModels_mm10
+#' @name geneModels_hg19
+NULL
+
+#' Gene models
+#'
+#' A dataset containing chromosome sizes for common reference genome assemblies
+#'
+#' @format A named vectors of lengths with one item per chromosome
+#' @source BSGenome package
+#' @name geneModels_hg38
 NULL
 
 #' Gene models
@@ -87,5 +105,5 @@ NULL
 #'
 #' @format A named vectors of lengths with one item per chromosome
 #' @source BSGenome package
-#' @name geneModels_mm10
+#' @name geneModels_mm9
 NULL
