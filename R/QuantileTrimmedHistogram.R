f906922e65496bb017e2e618c9f1f22479e2ae68
Tessa Danehy <49349383+tdanehy@users.noreply.github.com>
2019-12-12 18:58:26 -0500

    End of semester QT-hist
    I was able to add commas to the axis by editing the labelCuts function. I don't know if you want that to be changed for all graphs or just mine- I can re-name the function something else if you want to keep them separate. 
    
    This function operates with the following constraints: can't have a quantile less than 1 or over 20. The automated options return between 5 and 100 bins and that value is found based on the size of the dataset. The automated quantile for the smallest data (n less than 500) is 5% on each side of the graph, and for the largest data (n over 10,000) the automated value is 1%.

--- R/QuantileTrimmedHistogram.R
+++ R/QuantileTrimmedHistogram.R
@@ -4,137 +4,81 @@ calcWidth = function(query) {
 }
 
 ## plotting function
-plotQthist = function(df, EndBarColor = "maroon", MiddleBarColor = "gray57", quantile=NULL, bins=NULL) {
-  # if user gives no bins or quantiles, we calculate everything based on size of dataset
-  if(is.null(quantile) & is.null(bins)){
-    q = calcQuantiles(df) 
-    div = calcDivisions(df, q)
-  }
-  # if user gives both bins and quantiles, use their values entirely
-  if(!is.null(quantile) & !is.null(bins)){
-    q = quantile
-    div = calcDivisions(df, q, bins=bins)
-  }
-  # if user gives quantiles but no bins, we use their quantile to calculate the divisions
-  # causes nonsensical results when q is greater than 5
-  if(!is.null(quantile) & is.null(bins)){
-    q = quantile
-    div = calcDivisions(df, q)
-  }
-  # if user gives bins but no quantiles, we calculate quantiles then insert bin number into the calcDivisions function
-  if(is.null(quantile) & !is.null(bins)){
-    q = calcQuantiles(df) 
-    div = calcDivisions(df, q, bins= bins)
-  }
+plotQthist = function(df, EndBarColor = "gray57", MiddleBarColor = "gray27", quantile=NULL, bins=NULL) {
   
-  
-  colors_vect = c( EndBarColor , rep(MiddleBarColor, (length(div)-3)), EndBarColor) # creates a vector for the colors
-  
-  df = cutDists(df, divisions= div) # calculating a frequency table with the specified divisions
-  if ("name" %in% names(df)){
-    # It has multiple regions
-    g = ggplot(df, aes(x=cuts, y=Freq, fill=name)) + 
-      facet_grid(. ~name)
-  } else {
-    g = ggplot(df, aes(x=cuts, y=Freq))
-  }
-  
-  g = g +
-    geom_bar(stat="identity", fill = colors_vect) + 
-    theme_classic() + 
-    theme(aspect.ratio=1) + 
-    theme_blank_facet_label() +
-    ylab("Frequency") +
-    xlab("") +
-    theme(axis.text.x=element_text(angle = 90, hjust = 1, vjust=0.5)) + # vlab()
-    theme(plot.title = element_text(hjust = 0.5)) + # Center title
-    ggtitle("Quantile Trimmed Histogram") +
-    theme(legend.position="bottom") +
-    geom_text(aes(label= paste(q,"%", sep='')), data=df[c(1,length(df$Freq)),], vjust=-1)
-  return(g)
+      output = calcDivisions(df, quantile=quantile, bins=bins)
+      bins = output[["bins"]]
+      quantile = output[["quantile"]]
+      div = output[["divisions"]]
+      
+      colors_vect = c( EndBarColor , rep(MiddleBarColor, (length(div)-3)), EndBarColor) # creates a vector for the colors
+      
+      df = cutDists(df, divisions= div) # calculating a frequency table with the specified divisions
+      if ("name" %in% names(df)){
+        # It has multiple regions
+        g = ggplot(df, aes(x=cuts, y=Freq, fill=name)) + 
+          facet_grid(. ~name)
+      } else {
+        g = ggplot(df, aes(x=cuts, y=Freq))
+      }
+      
+      g = g +
+        geom_bar(stat="identity", fill = colors_vect) + 
+        theme_classic() + 
+        theme(aspect.ratio=1) + 
+        theme_blank_facet_label() +
+        ylab("Frequency") +
+        xlab("") +
+        theme(axis.text.x=element_text(angle = 90, hjust = 1, vjust=0.5)) + # vlab()
+        theme(plot.title = element_text(hjust = 0.5)) + # Center title
+        ggtitle("Quantile Trimmed Histogram") +
+        theme(legend.position="bottom") +
+        geom_text(aes(label= paste(quantile,"%", sep='')), data=df[c(1,length(df$Freq)),], vjust=-1)
+      return(g)
 }
 
+######################################## helper functions 
 
-######################################## helper functions
-
-############# calculating the quantiles
-calcQuantiles = function(df){
-  n = length(df) # finding number of observations
-  if (n > 1000) {n = 1000}
-  if (n < 100) {n = 100}
-  q = (11 - round(n/100)) # finding quantiles that should be used for end boxes
-  return(q)
-}
-
-############# calulating the divisions
-calcDivisions = function(df, q, bins=NULL){
-  q = as.numeric(q)
-  if(q>=50){
-    stop("Quantile can not be larger than 50. Optimal size is under 10.")
-  }
-  if(!is.null(bins)){
-    b = bins +1
-  } 
-  else {
-    b = ((20-(2*q))/q) # FIX THIS!!! finding the number of bins based on the quantiles
-  }
-  quant = unname(quantile(df, probs = c((q/100), (1-(q/100)))))
-  seq_10 = seq(quant[1], quant[2], length = b)
-  div = c(-Inf, round(seq_10), Inf)
-  return(div)
+############# calulating the divisions for the graph
+calcDivisions = function(df, bins=NULL, quantile = NULL){
+      # calculating bins
+      if(!is.null(bins)){
+        b = bins
+      } 
+      else {
+        n = length(df)
+        if (n > 10000) {n = 10000}
+        if (n < 500) {n = 500}
+        b = round(n^.15 + (n/200) ) # finding number of bins based on the size of dataset
+      }
+      # calculating quantiles
+      if(!is.null(quantile)){
+        if(quantile >= 20){
+          stop("Quantile can not be larger than 20. Optimal size is under 10.")
+        }
+        q = quantile
+      }
+      else{
+        q = round(25/(b)) # finding the quantile on each size based on number of bins
+        q = max(1,q) # minimum percentage on each side is 1%
+      }
+      quant = unname(quantile(df, probs = c((q/100), (1-(q/100)))))
+      seq_10 = seq(quant[1], quant[2], length = b+1)
+      div = c(-Inf, round(seq_10), Inf)
+      listOutput <- list("bins"= b,"quantile"= q, "divisions" = div)
+      return(listOutput)
 }
 
 ############ LABEL CUTS
-# need to put the comma function in labelCuts because it takes a numeric vector and produces character labels
+# I changed this helper function so that the x-axis would have commas
 labelCuts = function(breakPoints, digits=1, collapse="-", infBins=FALSE) {
-  labels = 
-    apply(round(cbind( breakPoints[-length(breakPoints)],	
-                       breakPoints[-1]),digits), 1, paste0, collapse=collapse) 
-  
-  if (infBins) {
-    labels[1] = paste0("<", breakPoints[2])
-    labels[length(labels)] = paste0(">", breakPoints[length(breakPoints)-1])
-  }
-  return(labels)
+      roundedLabels = signif(round(cbind( breakPoints[-length(breakPoints)],breakPoints[-1]),digits), 3)
+      is.na(roundedLabels) <- sapply(roundedLabels, is.infinite) #set the Inf values to NA so that formatC could add commas
+      labelsWithCommas = formatC(roundedLabels, format="d", big.mark=",")
+      labels = apply(labelsWithCommas, 1, paste0, collapse=collapse) 
+      if (infBins) {
+        labels[1] = paste0("<", formatC(breakPoints[2], format="d", big.mark=","))
+        labels[length(labels)] = paste0(">", formatC(breakPoints[length(breakPoints)-1], format="d", big.mark=","))
+      }
+      return(labels)
 }
-
-############## CUT DISTANCES
-### This function calls label cuts
-cutDists = function(dists, divisions = c(-Inf, -1e6, -1e4, -1000, -100, 0, 100, 1000, 10000, 1e6, Inf)) {
-  if (is.list(dists)) {
-    x = lapply(dists, cutDists)
-    
-    # To accommodate multiple lists, we'll need to introduce a new 'name'
-    # column to distinguish them.
-    nameList = names(dists)
-    if(is.null(nameList)) {
-      nameList = 1:length(query) # Fallback to sequential numbers
-    }
-    
-    # Append names
-    xb = rbindlist(x)
-    xb$name = rep(nameList, sapply(x, nrow))
-    
-    return(xb)
-  }
-  
-  labels = labelCuts(signif(divisions,3), collapse=" to ", infBins=TRUE)
-  cuts = cut(dists, divisions, labels)
-  df = as.data.frame(table(cuts))
-  return(df)
-}
-
-
-######### CHANGING THEME
-theme_blank_facet_label = function() {
-  return(theme(
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    strip.background = element_blank()
-  )
-  )
-}
-
-
-
-
