2badd7b18a02d049de0fe62280e07a703749662d
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-03-26 12:50:35 -0400

    test open chrom multi; #21

--- tests/testthat/testOpenChromatin.R
+++ tests/testthat/testOpenChromatin.R
@@ -7,13 +7,18 @@ exampleCellMatrixFile = system.file("extdata", "example_cell_matrix.txt.gz", pac
 cellMatrix = data.table::fread(exampleCellMatrixFile)
 f = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
 query = rtracklayer::import(f)
-querySftd = GenomicRanges::shift(query, 1000000)
+querySftd = GenomicRanges::shift(query, 100000)
+queryList = GRangesList(q1=query, q2=querySftd)
 
 # tests
 context("general")
 test_that("calcOpenSignal works", {
     expect_visible(calcOpenSignal(query, cellMatrix))
     expect_visible(calcOpenSignal(querySftd, cellMatrix))
+})
+
+test_that("calcOpenSignal works with multiple queries", {
+    expect_visible(calcOpenSignal(queryList, cellMatrix))
 })
 
 context("result")
@@ -24,3 +29,9 @@ test_that("calcOpenSignal returns a proper result of a proper class", {
 test_that("calcOpenSignal different results for different queries", {
     expect_false(NROW(calcOpenSignal(query, cellMatrix))==NROW(calcOpenSignal(querySftd, cellMatrix)))
 })
+
+test_that("calcOpenSignal combines results from multi-query runs", {
+    ql = GRangesList(q1=query, q2=query)
+    expect_true(NROW(calcOpenSignal(query, cellMatrix))*2==NROW(calcOpenSignal(ql, cellMatrix)))
+})
+
