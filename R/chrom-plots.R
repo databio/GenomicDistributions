110ad151cdddba77f48b56b42266315d4745429b
nsheff <nsheff@users.noreply.github.com>
2020-03-26 13:52:13 -0400

    fix horizontal chrom plot issue

--- R/chrom-plots.R
+++ R/chrom-plots.R
@@ -218,7 +218,8 @@ plotChromBins = function(genomeAggregate, binCount=10000, plotTitle="Distributio
 		theme(panel.spacing=unit(0, "lines")) + # Reduce whitespace
 		theme(strip.text.y=element_text(size=12, angle=0)) + # Rotate labels
 		geom_hline(yintercept=0, color="#EEEEEE") + # Light chrom lines
-		scale_y_discrete(breaks=c(0, max(genomeAggregate$N)), limits=c(max(genomeAggregate$N))) + 
+		# scale_y_discrete(breaks=c(0, max(genomeAggregate$N)), limits=c(max(genomeAggregate$N))) + 
+		scale_y_continuous(breaks=c(max(genomeAggregate$N)), limits=c(0, max(genomeAggregate$N))) +
 		scale_x_continuous(breaks=c(0, binCount), labels=c("Start", "End")) +
 		theme(plot.title=element_text(hjust=0.5)) + # Center title
 		ggtitle(plotTitle) +
