4fe6048dade42cdb025e5ec759cde2b195f35b4b
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-05-07 16:56:18 -0400

    add GDData vignette

--- long_vignettes/render-long-vignettes.R
+++ long_vignettes/render-long-vignettes.R
@@ -1,2 +1,4 @@
 knitr::opts_knit$set(base.dir = 'vignettes/', progress = TRUE, verbose = TRUE, fig.path="figures-full-power/")
 knitr::knit("long_vignettes/full-power.Rmd", "vignettes/full-power.Rmd")
+knitr::opts_knit$set(base.dir = 'vignettes/', progress = TRUE, verbose = TRUE, fig.path="figures-GDData/")
+knitr::knit("long_vignettes/GenomicDistributionsData.Rmd", "vignettes/GenomicDistributionsData.Rmd")
