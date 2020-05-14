fe4cba03b14ccf529e1fbe90be1f04697fd948b0
joseverdezoto <j.esiempreliga@hotmail.com>
2020-05-14 17:34:15 -0400

    add unit tests for new functions

---
+++ tests/testthat/testNeighborDist.R
@@ -0,0 +1,33 @@
+# lib
+library(data.table)
+library(testthat)
+library(GenomicDistributions)
+
+# data
+query = vistaEnhancers
+querySftd = GenomicRanges::shift(query, 100000)
+queryList = GRangesList(q1=query, q2=querySftd)
+
+# tests
+context("general")
+test_that("calcNeighborDist works", {
+  lapply(queryList, function(x) expect_visible(calcNeighborDist(x)))
+})
+
+context("result")
+test_that("calcNeighborDist returns a result of a proper class", {
+  expect_true(is(calcNeighborDist(query), "numeric"))
+  expect_true(is(calcNeighborDist(queryList), "list" ))
+})
+
+test_that("calcNeighborDist returns the same result for a shifted region set", {
+  expect_equal(calcNeighborDist(query), calcNeighborDist(querySftd))
+})
+
+ test_that("calcNeighborDist yields a numeric in range 0-10", {
+     x = calcNeighborDist(query)
+     for(i in x){
+         expect_gt(i, 0)    
+         expect_lt(i, 10)
+     }
+})
