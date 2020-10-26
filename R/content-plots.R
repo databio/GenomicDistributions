69149731b9cf72929f42bdfe6006770c10b9200d
John S <jstubbsiv@gmail.com>
2020-10-26 13:14:12 -0400

    added function calls and fixed ref func - test

--- R/content-plots.R
+++ R/content-plots.R
@@ -38,6 +38,39 @@ calcGCContent = function(query, ref) {
 
 #' Calculate GC content over genomic ranges
 #' 
+#' Calculate GC content over genomic ranges
+#' 
+#' Given a reference genome as a BSgenome object and some ranges on that
+#' reference, this function will return a vector of the same length as the
+#' granges object, with percent of Cs and Gs.
+#' 
+#' @param query  A GenomicRanges or GenomicRangesList object with query regions.
+#' @param ref Reference genome BSgenome object.
+#' @return A numeric vector of list of vectors with the GC percentage of the query regions.
+#' @export
+#' @examples
+#' \dontrun{
+#'query = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
+#'GRquery = rtracklayer::import(query)
+#'bsg = loadBSgenome('hg19')
+#'gcvec = calcGCContent(GRquery, bsg)
+#' }
+calcGCContent = function(query, ref) {
+    .validateInputs(list(query=c("GRanges","GRangesList"),
+                         ref="BSgenome"))
+    if (is(query, "GRangesList")) {
+        # Recurse over each GRanges object
+        x = lapply(query, calcGCContent, ref)
+        return(x)
+    }
+    v = IRanges::Views(ref, query)
+    gcvec = apply(Biostrings::alphabetFrequency(v)[,c("C","G")],1, sum)/width(v)
+    return(gcvec)
+}
+
+
+#' Calculate GC content over genomic ranges
+#' 
 #' Given a reference genome as a BSgenome object and some ranges on that
 #' reference, this function will return a vector of the same length as the
 #' granges object, with percent of Cs and Gs.
@@ -46,13 +79,14 @@ calcGCContent = function(query, ref) {
 #' @param refAssembly A character vector specifying the reference genome
 #'     assembly (*e.g.* 'hg19'). This will be used to grab chromosome sizes with
 #'     \code{getTSSs}.
-#' @return A numeric vector or list of vectors with the GC percentage of 
-#'     the query regions.
+#' @return A numeric vector or list of vectors with the GC percentage of the query regions.
 #' @export
 #' @examples
 #' \dontrun{
+#' query = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
+#' GRquery = rtracklayer::import(query)
 #' refAssembly = 'hg19'
-#' GCcontent = calcGCContentRef(vistaEnhancers, refAssembly)
+#' GCcontent = calcGCContentRef(GRquery, refAssembly)
 #' } 
 calcGCContentRef = function(query, refAssembly) {
     .validateInputs(list(query=c("GRanges","GRangesList"),
@@ -71,8 +105,7 @@ calcGCContentRef = function(query, refAssembly) {
 #' @examples
 #' numVector = rnorm(400, mean=0.5, sd=0.1)
 #' GCplot = plotGCContent(numVector)
-#' vecs = list(example1 = rnorm(400, mean=0.5, sd=0.1), 
-#'             example2 = rnorm(600, mean=0.5, sd=0.1))
+#' vecs = list(example1 = rnorm(400, mean=0.5, sd=0.1), example2 = rnorm(600, mean=0.5, sd=0.1))
 #' GCplot = plotGCContent(numVector)
 #' 
 plotGCContent = function(gcvectors) {
@@ -83,21 +116,20 @@ plotGCContent = function(gcvectors) {
     colnames(gcdfReshaped)[colnames(gcdfReshaped) == "L1"] = "regionSet"
     # plot multiple regionsets if gcvectors is a list
     if (is(gcvectors, "list")) {
-        meansdf = aggregate(gcdfReshaped$value, 
-                            list(gcdfReshaped$regionSet), mean)
+        meansdf = aggregate(gcdfReshaped$value, list(gcdfReshaped$regionSet), mean)
         g = ggplot2::ggplot(gcdfReshaped, aes(x=value, colour=regionSet)) +
-        geom_density() +
-        geom_vline(data=meansdf, aes(xintercept=x, colour=Group.1),
-                   linetype="dashed", size=0.5) +
-        theme_classic() +
-        theme(legend.position = "bottom")
+            geom_density() +
+            geom_vline(data=meansdf, aes(xintercept=x, colour=Group.1),
+                       linetype="dashed", size=0.5) +
+            theme_classic() +
+            theme(legend.position = "bottom")
     } else {
         # plot a single regionset
         g = ggplot2::ggplot(gcdfReshaped, aes(x=value)) + 
-        geom_density() + 
-        geom_vline(aes(xintercept=mean(value)),
-                   color="red", linetype="dashed", size=0.5) + 
-        theme_classic()
+            geom_density() + 
+            geom_vline(aes(xintercept=mean(value)),
+                       color="red", linetype="dashed", size=0.5) + 
+            theme_classic()
     }    
     g = g + 
         ggtitle("GC content distribution") + 
@@ -106,7 +138,6 @@ plotGCContent = function(gcvectors) {
         xlim(0,1) 
     return(g)
 }
-
 
 #' Calculate Dinuclotide content over genomic ranges
 #' 
@@ -130,75 +161,31 @@ calcDinuclFreq = function(query, ref) {
                          ref="BSgenome"))
     if (is(query, "GRangesList")) {
         
-        x = lapply(query, calcDinuclFreq, ref)
-    }
-        namelist=names(query)
+        # Recurse over each GRanges object
         
-        if (is.null(namelist)){
+        x = lapply(query, calcDinuclFreq, ref)
+        
+        namelist = names(query)
+        
+        if (is.null(namelist)) {
             
-            newnames=seq_along(query)
+            newnames = seq_along(query)
             
-            namelist=newnames
+            namelist = newnames
             
-            names(x)=namelist
+            # Append names
+            
+            names(x) = namelist
         }
         return(x)
-    
-    
+    }
     seqlevels(query, pruning.mode="coarse")=seqlevels(ref)
     
     v = IRanges::Views(ref, query)
     
-    dnvec= as.data.table(Biostrings::dinucleotideFrequency(v))
+    DNFdf= data.table::as.data.table(Biostrings::dinucleotideFrequency(v))
     
-    return(dnvec)
-    }
-
-
-
-#' Plot dinuclotide content over genomic ranges
-#' 
-#' Given \code{calcDinuclFreq} results, this function 
-#' generates a violin plot of dinucleotide frequency
-#' 
-#' @param DNFDataTable A data.table of dinucleotide counts 
-#' @return A ggplot object plotting distribution of dinucleotide content per dinucleotide
-#' @export
-#' @examples
-#' 
-#' DNFDataTable = data.table::data.table(GC = rnorm(400, mean=0.5, sd=0.1), 
-#' CG = rnorm(400, mean=0.5, sd=0.5), 
-#' AT = rnorm(400, mean=0.5, sd=1), 
-#' TA = rnorm(400, mean=0.5, sd=1.5))
-#' 
-#' DNFPlot =  plotDinuclFreq(DNFDataTable)
-#' 
-plotDinuclFreq = function(DNFDataTable) {
-    
-    .validateInputs(list(DNFDataTable=c("matrix", "array", 
-                                        "data.frame", "data.table")))
-    
-    
-    g = DNFDataTable
-    
-    ## for violinplot
-    
-    g=reshape2::melt(g) 
-    
-    names(g)[names(g)=="variable"]="dinucleotide"
-    
-    names(g)[names(g)=="value"]="frequency"
-    
-    g$frequency=as.numeric(g$frequency)
-    
-    g$dinucleotide=as.character(g$dinucleotide)
-    
-    plot=ggplot(data=g, aes(dinucleotide, frequency)) + 
-        geom_violin(scale="width", trim=TRUE) + 
-        geom_boxplot(width=0.1, color="grey", alpha=0.2) + 
-        coord_flip() + ggtitle("Dinucleotide Frequency") 
-    
-    return(plot)
+    return(DNFdf)
 }
 
 #' Calculate dinucleotide content over genomic ranges
@@ -216,19 +203,66 @@ plotDinuclFreq = function(DNFDataTable) {
 #' @export
 #' @examples
 #' \dontrun{
-#' refAssembly = 'hg19'
-#' GCcontent = calcGCContentRef(vistaEnhancers, refAssembly)
+#'query = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
+#'GRquery = rtracklayer::import(query)
+#'refAssembly = 'hg19'
+#'DNF = calcDinuclFreqRef(GRquery, refAssembly)
 #' } 
-
 
 calcDinuclFreqRef= function(query, refAssembly) {
     
-    .validateInputs(list(query=c("GRanges","GRangesList"),
-                         
-                         refAssembly="character"))
+    GenomicDistributions:::.validateInputs(list(query=c("GRanges","GRangesList"),
+                                                
+                                                refAssembly="character"))
     
     ref = loadBSgenome(refAssembly)
     
     return(calcDinuclFreq(query, ref))
 }
 
+#' Plot dinuclotide content over genomic ranges
+#' 
+#' Given \code{calcDinuclFreq} results, this function 
+#' generates a violin plot of dinucleotide frequency
+#' 
+#' @param DNFDataTable A data.table of dinucleotide counts 
+#' @return A ggplot object plotting distribution of dinucleotide content per dinucleotide
+#' @export
+#' @examples
+#' 
+#' DNFDataTable = data.table::data.table(GC = rnorm(400, mean=0.5, sd=0.1), 
+#' CG = rnorm(400, mean=0.5, sd=0.5), 
+#' AT = rnorm(400, mean=0.5, sd=1), 
+#' TA = rnorm(400, mean=0.5, sd=1.5))
+#' 
+#' DNFPlot =  plotDinuclFreq(DNFDataTable)
+
+
+plotDinuclFreq = function(DNFDataTable) {
+    
+    GenomicDistributions:::.validateInputs(list(DNFDataTable=c("matrix", "array", 
+                                                               "data.frame", "data.table")))
+    
+    
+    g = DNFDataTable
+    
+    ## for violinplot
+    
+    g=reshape2::melt(g) 
+    
+    names(g)[names(g)=="variable"]="dinucleotide"
+    
+    names(g)[names(g)=="value"]="frequency"
+    
+    g$frequency=as.numeric(g$frequency)
+    
+    g$dinucleotide=as.character(g$dinucleotide)
+    
+    plot=ggplot2::ggplot(data=g, ggplot2::aes(dinucleotide, frequency))+
+        ggplot2::geom_violin(scale="width", trim=TRUE)+
+        ggplot2::geom_boxplot(width=0.1, color="grey", alpha=0.2)+
+        ggplot2::coord_flip()+
+        ggplot2::ggtitle("Dinucleotide Frequency") 
+    
+    return(plot)
+}
